32ebb9162bfb71ef8272934dcc83d0c77e4c86ac
mryunt02 <mrthresh58@gmail.com>
2024-01-03 04:19:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,81 +1,91 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import "./index.css";
 import Header from './Header';
 import AddTask from './AddTask';
 import { v4 as uuidv4 } from "uuid";
-import EasyEdit, { Types } from "react-easy-edit";
-import DeleteButton from './DeleteButton';
 function App(props) {
-  const [schedule, setSchedule] = useState(
-    [
-      {
-        id: 1,
-        name: "Calculus Final exam",
-        endtime: "13 January",
+  /*const [schedule, setSchedule] = useState(
+   
+ )*/
+  const [schedule, setSchedule] = useState(() => {
+    const savedSchedule =
+      localStorage.getItem('schedule');
+    return savedSchedule ? JSON.parse(savedSchedule) :
+      [
+        {
+          id: 1,
+          name: "Calculus Final exam",
+          endtime: "13 January",
 
-      },
-      {
-        id: 2,
-        name: "Physics Final exam",
-        endtime: "17 January ",
+        },
+        {
+          id: 2,
+          name: "Physics Final exam",
+          endtime: "17 January ",
 
-      },
-      {
-        id: 3,
-        name: "Chemistry Final exam",
-        endtime: "18 January",
+        },
+        {
+          id: 3,
+          name: "Chemistry Final exam",
+          endtime: "18 January",
 
-      },
-      {
-        id: 4,
-        name: "Digital Electronics Final exam",
-        endtime: "18 January",
+        },
+        {
+          id: 4,
+          name: "Digital Electronics Final exam",
+          endtime: "18 January",
 
-      },
-      {
-        id: 5,
-        name: "Programming Languages Final exam",
-        endtime: "20 January",
+        },
+        {
+          id: 5,
+          name: "Programming Languages Final exam",
+          endtime: "20 January",
 
-      },
-      {
-        id: 6,
-        name: "Operating Systems Final exam",
-        endtime: "18 January",
+        },
+        {
+          id: 6,
+          name: "Operating Systems Final exam",
+          endtime: "18 January",
 
-      },
-      {
-        id: 7,
-        name: "Internet Of Things Final exam",
-        endtime: "16 January",
+        },
+        {
+          id: 7,
+          name: "Internet Of Things Final exam",
+          endtime: "16 January",
 
-      },
-      {
-        id: 8,
-        name: "Microprocessors Final exam",
-        endtime: "16 January",
+        },
+        {
+          id: 8,
+          name: "Microprocessors Final exam",
+          endtime: "16 January",
 
-      },
-      {
-        id: 9,
-        name: "Databases Final exam",
-        endtime: "14 January",
+        },
+        {
+          id: 9,
+          name: "Databases Final exam",
+          endtime: "14 January",
 
-      },
-      {
-        id: 10,
-        name: "Differential equations Final exam",
-        endtime: "14 January",
+        },
+        {
+          id: 10,
+          name: "Differential equations Final exam",
+          endtime: "14 January",
 
-      },
+        },
 
 
-    ]
-  )
+      ]
+  })
+
+
+  useEffect(() => {
+    localStorage.setItem('schedule', JSON.stringify(schedule))
+
+  }, [schedule])
 
   function updateFutbol(id, newName, newendtime) {
     const updatedFutboller = schedule.map((sch) => {
-      if (id == sch.id) {
+      if (id === sch.id) {
         return { ...sch, name: newName, endtime: newendtime }
       }
       return sch;
--- src/Header.js
+++ src/Header.js
@@ -9,9 +9,10 @@ function Header(props) {
     return (
         <div class="min-w-[350px] max-w-[350px]m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 mt-6 flex-col">
 
-            <div class="text-center space-y-2 sm:text-left ">
+            <div class="text-center space-y-2 sm:text-left">
                 <DeleteButton
                     deleteExam={props.deleteExam}
+                    saveToStorage={props.saveToStorage}
                     id={props.id} />
                 <div class="space-y-0.5">
                     <p class="text-lg text-black font-semibold">
@@ -27,7 +28,6 @@ function Header(props) {
                     endtime={props.endtime}
                     updateFutbol={props.updateFutbol}
                 />
-
             </div>
         </div>
 
